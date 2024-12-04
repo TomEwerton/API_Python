4943ebae6eeb4ae79587b9535f0a3305c699de8b
Éwerton Jamebrg Ribeiro <startronic1793@gmail.com>
2024-12-03 21:30:38 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# inicialmente deve-se cirar um ambiente virtual (no terminal: python -m venv venve)
+# ativar o ambiente virtual (no terminal: .\venv\Scripts\activate)
+# baixar os pacotes fastapi e uvicorn
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+vendas = {
+    1: {"item": "lata", "preco_unitario": 4, "quantidade": 5},
+    2: {"item": "garrafa 2L", "preco_unitario": 15, "quantidade": 5},
+    3: {"item": "garrafa 750ml", "preco_unitario": 10, "quantidade": 5},
+    4: {"item": "lata mini", "preco_unitario": 2, "quantidade": 5},
+}
+
+# criando rotas
+
+
+@app.get("/")
+def home():
+    return {"Vendas": len(vendas)}
+
+
+@app.get("/vendas/{id_venda}")
+def pegar_venda(id_venda: int):
+    if id_venda in vendas:
+        return vendas[id_venda]
+    else:
+        return {"Erro: ID Venda inexistenete"}
